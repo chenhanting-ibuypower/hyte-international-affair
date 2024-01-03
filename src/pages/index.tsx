2f2e2066eee07139902405361d2108a3cdd1e088
Han <han-chen@ibuypower.com>
2024-01-03 16:43:27 +0800

    fix: forsake getserversideprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,32 +2,6 @@ import React, { useState, useEffect } from "react";
 import FeedbackTable from "../components/FeedbackTable";
 import AddButton from "../components/AddButton";
 import Pagination from "../components/Pagination";
-import { MongoClient } from "mongodb";
-import { GetServerSideProps } from "next";
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    const client = new MongoClient(process.env.MONGODB_URL as string);
-    await client.connect();
-    const collection = client.db("localization").collection("translations");
-    const documents = await collection.find({}).toArray();
-    client.close();
-
-    return {
-      props: {
-        translations: JSON.parse(JSON.stringify(documents)),
-      },
-    };
-  } catch (err) {
-    console.error("Error fetching data:", err);
-    return {
-      props: {
-        translations: [], // Provide an empty array if there's an error
-        error: "Failed to fetch data", // You can pass error messages as props as well
-      },
-    };
-  }
-};
 
 const IndexPage = ({ translations }: { translations?: Array<object> }) => {
   console.log("📕 (Client Side):", translations);
@@ -36,7 +10,7 @@ const IndexPage = ({ translations }: { translations?: Array<object> }) => {
   const [totalPages, setTotalPages] = useState(0);
   const [totalDocuments, setTotalDocuments] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  
+
   const handlePageChange = (pageNumber: number) => {
     // Update the current page here
     console.log("Current Page:", pageNumber);
